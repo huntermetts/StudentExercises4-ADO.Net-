c0f570599da21d02e43df6884360dbb99b6f276d
unknown <huntermettsdev@gmail.com>
2019-03-20 12:37:17 -0500

    Number 1

---
+++ StudentExercisesADO.NET/Data/Repository.cs
@@ -0,0 +1,70 @@
+﻿using StudentExercisesADO.NET.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace StudentExercisesADO.NET
+{
+    public class Repository
+    {
+        public SqlConnection Connection
+        {
+            get
+            {
+                // This is "address" of the database
+                string _connectionString = "Server=DESKTOP-6QUVPEQ\\SQLEXPRESS;database=StudentExercisesDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                return new SqlConnection(_connectionString);
+            }
+        }
+        
+
+
+        public List<Exercise> GetAllExercises()
+        {
+         
+            using (SqlConnection conn = Connection)
+            {
+                
+                conn.Open();
+                
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+    
+                    cmd.CommandText = "SELECT Id, Name, Language FROM Exercise";
+                   
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    
+                    List<Exercise> exercises = new List<Exercise>();
+                    
+                    while (reader.Read())
+                    {
+                       
+                        int idColumnPosition = reader.GetOrdinal("Id");
+                        
+                        int idValue = reader.GetInt32(idColumnPosition);
+                        int exerciseNameColumnPosition = reader.GetOrdinal("name");
+                        int exerciseLanguageColumnPosition = reader.GetOrdinal("language");
+                        string exerciseNameValue = reader.GetString(exerciseNameColumnPosition);
+                        string exerciseLanguageValue = reader.GetString(exerciseLanguageColumnPosition);
+
+                        Exercise exercise = new Exercise
+                        {
+                            Id = idValue,
+                            Name = exerciseNameValue,
+                            Language = exerciseLanguageValue
+                        };
+
+                        exercises.Add(exercise);
+                    }
+                    
+                    reader.Close();
+                    return exercises;
+                }
+            }
+        }
+     }
+}
+    
+    
+
---
+++ StudentExercisesADO.NET/Program.cs
@@ -0,0 +1,26 @@
+﻿using StudentExercisesADO.NET.Models;
+using System;
+using System.Collections.Generic;
+
+namespace StudentExercisesADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Repository repository = new Repository();
+            List<Exercise> exercises = repository.GetAllExercises();
+            
+
+            foreach(Exercise exercise in exercises)
+            {
+                Console.WriteLine($"The exercise name is {exercise.Name} and the language is {exercise.Language}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
